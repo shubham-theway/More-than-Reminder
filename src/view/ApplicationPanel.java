8956230f47e85bb8faa80846206a9041f4a8a6bd
shubham-theway <dave.shubham@gmail.com>
2012-04-23 09:27:42 +0530

    changes related to Notes

--- src/view/ApplicationPanel.java
+++ src/view/ApplicationPanel.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,6 +16,8 @@ import javax.swing.JPopupMenu;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 
+import app.Application;
+
 import com.melloware.jintellitype.HotkeyListener;
 import com.melloware.jintellitype.JIntellitype;
 
@@ -22,8 +25,9 @@ public class ApplicationPanel extends JFrame implements ActionListener // ,Mouse
 , HotkeyListener
 {
     private JPopupMenu rightMenu;
-    private JMenuItem rightMenuItem; 
+    private JMenuItem addReminderMenuItem,addNoteMenuItem; 
     private AddReminderFrame addRem;
+    private AddNoteFrame addNote;
     private int lastX,lastY;
     // hot key
     JIntellitype jintel;
@@ -31,17 +35,19 @@ public class ApplicationPanel extends JFrame implements ActionListener // ,Mouse
     {
 	this.setTitle("Reminder");
 	rightMenu=new JPopupMenu();
-	rightMenuItem=new JMenuItem("Create Reminder");
-	rightMenuItem.addActionListener(this);
+	addReminderMenuItem=new JMenuItem("Create Reminder");
+	addNoteMenuItem=new JMenuItem("Add Note");	
+	addReminderMenuItem.addActionListener(this);
+	addNoteMenuItem.addActionListener(this);
 	//hotkey
 	jintel=JIntellitype.getInstance();
 	jintel.registerHotKey(1, JIntellitype.MOD_ALT + JIntellitype.MOD_SHIFT, (int)'S');
 	//jintel.registerHotKey(2, JIntellitype.MOD_ALT + JIntellitype.MOD_SHIFT, (int)'B');
 
-        //or you can use the Swing constants instead
+	//or you can use the Swing constants instead
 	//jintel.registerSwingHotKey(3, Event.CTRL_MASK + Event.SHIFT_MASK, (int)'C');
-        //this.addWindowListener(this);
-        // To unregister them just call unregisterHotKey with the unique identifier
+	//this.addWindowListener(this);
+	// To unregister them just call unregisterHotKey with the unique identifier
 	/*jintel.unregisterHotKey(1);
 	jintel.unregisterHotKey(2);
 	jintel.unregisterHotKey(3);*/
@@ -54,25 +60,37 @@ public class ApplicationPanel extends JFrame implements ActionListener // ,Mouse
 	scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 	scrollPane.setHorizontalScrollBar(new JScrollBar(JScrollBar.HORIZONTAL));
 	scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-	*/// scroll
+	 */// scroll
     }
 
     public void actionPerformed(ActionEvent ae)
     {
 	System.out.println(ae.getActionCommand());
+	if(ae.getActionCommand().contains("Reminder"))
+	{
 	addRem=new AddReminderFrame(lastX,lastY);
 	addRem.setVisible(true);
+	}
+	else if(ae.getActionCommand().contains("Note"))
+	{
+	    addNote=new AddNoteFrame(lastX, lastY);
+	    addNote.setVisible(true);
+	}
     }
     public void showPopup(MouseEvent invoker)
     {
 	// TODO Auto-generated method stub
 	System.out.println("in app panel");
-	rightMenu.add(rightMenuItem);
+	rightMenu.add(addReminderMenuItem);
+	rightMenu.add(addNoteMenuItem);
 	lastX=invoker.getXOnScreen();
 	lastY=invoker.getYOnScreen();
-	
-	rightMenu.show(invoker.getComponent(), invoker.getX(), invoker.getY());
-	
+	Component c=invoker.getComponent();
+
+	rightMenu.show(c, invoker.getX(), invoker.getY());
+	System.out.println("invoker = "+c.getClass().getName().toString());
+	//rightMenu.show(c, invoker.getX(), invoker.getY());
+
     }
 
     @Override
@@ -80,7 +98,7 @@ public class ApplicationPanel extends JFrame implements ActionListener // ,Mouse
     {
 	// TODO Auto-generated method stub
 	if (aIdentifier == 1)
-            this.setVisible(true);
+	    this.setVisible(true);
     }
 }
 
