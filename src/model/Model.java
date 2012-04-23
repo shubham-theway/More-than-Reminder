e9b13855571704a7ef33de11e3bc34aa77acef71
shubham-theway <dave.shubham@gmail.com>
2012-04-23 09:22:26 +0530

    updated to include Notes

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,15 +1,15 @@
 package model;
 
 import java.io.*;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Observable;
 
+import view.AddNoteFrame;
 import view.AddReminderFrame;
 import model.*;
 
@@ -17,44 +17,80 @@ import model.*;
 public class Model extends Observable
 {
     private ReminderList reminderList;
-    private File reminderFile;
+    private NoteList noteList;
+    private File noteFile;
+    private File remFile;
     public Model()
     {
 	reminderList=(new ReminderList());
-	String name=System.getenv("APPDATA");
-	name+="\\reminders.sdv";
-	reminderFile=new File(name);
+	noteList=new NoteList();
+	String RemFileame=System.getenv("APPDATA");
+	RemFileame+="\\rem.sdv";
+	String noteFileName=System.getenv("APPDATA");
+	noteFileName+="\\note.sdv";
+	noteFile=new File(noteFileName);
+	remFile=new File(RemFileame);
 	deserializeListAndPopulateReminderList();
     }
     private void deserializeListAndPopulateReminderList()
     {
 	// TODO Auto-generated method stub
 	try
-        {
-	    if(!reminderFile.exists())
+	{
+	    if(!noteFile.exists())
 	    {
 		FileWriter writer;
-		writer=new FileWriter(reminderFile);
+		writer=new FileWriter(noteFile);
 		return;
 	    }
-           FileInputStream fileIn =
-                         new FileInputStream(reminderFile);
-           
-           ObjectInputStream in = new ObjectInputStream(fileIn);
-           reminderList = (ReminderList) in.readObject();
-           AddReminderFrame.setIndex(in.readInt());
-           in.close();
-           fileIn.close();
-       }catch(IOException i)
-       {
-           i.printStackTrace();
-           return;
-       }catch(ClassNotFoundException c)
-       {
-           System.out.println(ReminderList.class + " not found!");
-           c.printStackTrace();
-           return;
-       }
+	    FileInputStream fileIn =
+		new FileInputStream(noteFile);
+
+	    ObjectInputStream in = new ObjectInputStream(fileIn);
+	    noteList = (NoteList) in.readObject();
+	    AddNoteFrame.setIndex((Integer) in.readObject());
+	    //noteList=(NoteList)in.readObject();
+	    //AddNoteFrame.setIndex(in.readInt());
+	    in.close();
+	    fileIn.close();
+	}catch(IOException i)
+	{
+	    i.printStackTrace();
+	    return;
+	}catch(ClassNotFoundException c)
+	{
+	    System.out.println(ReminderList.class + " not found!");
+	    c.printStackTrace();
+	    return;
+	}
+	try
+	{
+	    if(!remFile.exists())
+	    {
+		FileWriter writer;
+		writer=new FileWriter(remFile);
+		return;
+	    }
+	    FileInputStream fileIn =
+		new FileInputStream(remFile);
+
+	    ObjectInputStream in = new ObjectInputStream(fileIn);
+	    reminderList = (ReminderList) in.readObject();
+	    AddReminderFrame.setIndex((Integer) in.readObject());
+	    //noteList=(NoteList)in.readObject();
+	    //AddNoteFrame.setIndex(in.readInt());
+	    in.close();
+	    fileIn.close();
+	}catch(IOException i)
+	{
+	    i.printStackTrace();
+	    return;
+	}catch(ClassNotFoundException c)
+	{
+	    System.out.println(ReminderList.class + " not found!");
+	    c.printStackTrace();
+	    return;
+	}
     }
     public void addReminder(ReminderData reminder)
     {
@@ -68,6 +104,7 @@ public class Model extends Observable
     public void removeReminder(int index)
     {
 	reminderList.removeReminder(index);
+	serializeList();
     }
     public void printReminders()
     {
@@ -91,11 +128,30 @@ public class Model extends Observable
 	try
 	{
 	    FileOutputStream fileOut =
-		new FileOutputStream(reminderFile);
+		new FileOutputStream(noteFile);
 	    ObjectOutputStream out =
 		new ObjectOutputStream(fileOut);
+	    //out.writeObject(this.reminderList);
+	    //out.writeInt(AddReminderFrame.getIndex());
+	    out.writeObject(this.noteList);
+	    out.writeObject(AddNoteFrame.getIndex());
+	    out.close();
+	    fileOut.close();
+	}catch(IOException i)
+	{
+	    System.out.print("error = ");
+	    i.printStackTrace();
+	}
+	try
+	{
+	    FileOutputStream fileOut =
+		new FileOutputStream(remFile);
+	    ObjectOutputStream out =
+		new ObjectOutputStream(fileOut);
+	    //out.writeObject(this.noteList);
+	    //out.writeInt(AddNoteFrame.getIndex());
 	    out.writeObject(this.reminderList);
-	    out.writeInt(AddReminderFrame.getIndex());
+	    out.writeObject(AddReminderFrame.getIndex());
 	    out.close();
 	    fileOut.close();
 	}catch(IOException i)
@@ -104,7 +160,7 @@ public class Model extends Observable
 	    i.printStackTrace();
 	}
     }
-    public ReminderData fetch(int ind)
+    public ReminderData fetchReminder(int ind)
     {
 	// TODO Auto-generated method stub
 	for(model.ReminderData rData : reminderList.observableList)
@@ -113,6 +169,96 @@ public class Model extends Observable
 		return rData;
 	}
 	return null;
+    }
+    public NoteData fetchNote(int ind)
+    {
+	// TODO Auto-generated method stub
+	for(model.NoteData nData : noteList.observableList)
+	{
+	    if(nData.getIndex()==ind)
+		return nData;
+	}
+	return null;
+    }
+    public List getTodays()
+    {
+	// TODO Auto-generated method stub
+	List todays=new LinkedList();
+	List remList=this.reminderList.observableList;
+	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+	Date date = new Date();
+	System.out.println("current = "+dateFormat.format(date));
+	for(Object r : remList)
+	{
+	    ReminderData rem=(ReminderData)r;
+	    String tdate=dateFormat.format(rem.getRemindAt()).toString();
+	    System.out.println(tdate);
+	    
+	    if(dateFormat.format(date).toString().equals(tdate))
+	    {
+		System.out.println("added!!");
+		todays.add(rem);
+	    }
+	}
+	return todays;
+    }
+    
+    public List getCompleted()
+    {
+	List completed=new LinkedList();
+	List remList=this.reminderList.observableList;
+	//DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+	Date date = new Date();
+	System.out.println("current = "+date);
+	for(Object r : remList)
+	{
+	    ReminderData rem=(ReminderData)r;
+	    Date tdate=rem.getRemindAt();
+	    System.out.println(tdate);
+	    
+	    if(date.after(tdate))
+	    {
+		System.out.println("added!!");
+		completed.add(rem);
+	    }
+	}
+	return completed;
+    }
+    
+    public List getIncomplete()
+    {
+	List incomplete=new LinkedList();
+	List remList=this.reminderList.observableList;
+	//DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+	Date date = new Date();
+	System.out.println("current = "+date);
+	for(Object r : remList)
+	{
+	    ReminderData rem=(ReminderData)r;
+	    Date tdate=rem.getRemindAt();
+	    System.out.println(tdate);
+	    
+	    if(date.before(tdate))
+	    {
+		System.out.println("added!!");
+		incomplete.add(rem);
+	    }
+	}
+	return incomplete;
+    }
+    public void addNote(String text, String sub)
+    {
+	// TODO Auto-generated method stub
+	if((text==null || text.length()==0) && (sub==null || sub.length()==0))
+	    return;
+	NoteData note=new NoteData(text,sub);
+	this.noteList.addNote(note);
+	serializeList();
+    }
+    public List<NoteData> getNotes()
+    {
+	// TODO Auto-generated method stub
+	return noteList.observableList;
     }
 
 }
